df48ef4eb5f4740c76a96502b61ee8f75af26e6e
Sebastian Fischer <typo3@evoweb.de>
2019-12-29 10:42:35 +0100

    Restructure installations scripts

--- Setup/raspberry4b.sh
+++ Setup/raspberry4b.sh
@@ -1,46 +1,77 @@
 #!/bin/bash
-loadkeys de
 
-wget http://archive.raspbian.org/raspbian.public.key -O - | apt-key add -
-dpkg --add-architecture armhf
-apt update
-apt install rpi-eeprom rpi-eeprom-image
+function add_missing_repository_keys() {
+  curl -fsSL http://archive.raspbian.org/raspbian.public.key | apt-key add -
 
-apt install git dhcpcd5
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-add-apt-repository ppa:ubuntu-raspi2/ppa
-sed -i '' 's/eoan/bionic/' /etc/apt/sources.list.d/ubuntu-raspi2-ubuntu-ppa-eoan.list
-mv /etc/apt/sources.list.d/ubuntu-raspi2-ubuntu-ppa-eoan.list /etc/apt/sources.list.d/ubuntu-raspi2-ubuntu-ppa-bionic.list
-apt install libraspberrypi-bin
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C
 
-cp ./Setup/Configuration/usb.sh /root/
-chmod 755 /root/usb.sh
+  dpkg --add-architecture armhf
+  apt update
+}
 
-systemctl stop systemd-resolved
-systemctl disable systemd-resolved
-rm -v /etc/resolv.conf
-cp ./Setup/Configuration/resolv.conf /etc/
-apt install dnsmasq
-cp ./Setup/Configuration/dnsmasq /etc/dnsmasq.d/
-echo "denyinterfaces usb0" >> /etc/dhcpcd.conf
-systemctl restart dnsmasq
+function install_raspberry_components() {
+  apt install rpi-eeprom rpi-eeprom-image
 
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-cat <<EOT >> /etc/apt/sources.list.d/docker-ubuntu-disco.list
-deb https://download.docker.com/linux/ubuntu disco stable
-# deb-src https://download.docker.com/linux/ubuntu disco stable
+  add-apt-repository ppa:ubuntu-raspi2/ppa
+  sed -i '' 's/eoan/bionic/' /etc/apt/sources.list.d/ubuntu-raspi2-ubuntu-ppa-eoan.list
+  mv /etc/apt/sources.list.d/ubuntu-raspi2-ubuntu-ppa-eoan.list /etc/apt/sources.list.d/ubuntu-raspi2-ubuntu-ppa-bionic.list
+  apt install libraspberrypi-bin
+
+  cp ./Setup/Configuration/usb.sh /root/
+  chmod 755 /root/usb.sh
+}
+
+function install_dnsmasq() {
+  systemctl stop systemd-resolved
+  systemctl disable systemd-resolved
+  systemctl mask systemd-resolved
+
+  rm -v /etc/resolv.conf
+  cp ./Setup/Configuration/resolv.conf /etc/
+
+  apt install git dhcpcd5
+  echo 'denyinterfaces usb0' >> /etc/dhcpcd.conf
+
+  apt install dnsmasq
+  cp ./Setup/Configuration/dnsmasq /etc/dnsmasq.d/
+  systemctl restart dnsmasq
+}
+
+function install_docker() {
+  cat <<EOT >> /etc/apt/sources.list.d/docker-ubuntu-disco.list
+	deb https://download.docker.com/linux/ubuntu disco stable
+	# deb-src https://download.docker.com/linux/ubuntu disco stable
 EOT
 
-apt install docker-ce
-usermod -aG docker ubuntu
+  apt update
+  apt install apt-transport-https ca-certificates curl software-properties-common
+  apt install docker-ce
+  usermod -aG docker ubuntu
+}
 
-echo "alias composer='[ -d ~/.composer ] || mkdir ~/.composer; docker run --rm --interactive --tty -u $UID -v `pwd`:/app -v ~/.composer:/tmp/.composer -e COMPOSER_HOME=/tmp/.composer composer --ignore-platform-reqs'" >> '/home/ubuntu/.bashrc'
+function install_docker_compose() {
+  apt update
+  apt install libffi-dev libssl-dev
+  apt install -y python python-pip python-dev
+  apt remove python-configparser
+  pip install docker-compose
+}
 
-apt install libffi-dev libssl-dev
-apt install -y python python-pip python-dev
-apt remove python-configparser
-pip install docker-compose
+function add_composer_alias() {
+  echo "alias composer='[ -d ~/.composer ] || mkdir ~/.composer; docker run --rm --interactive --tty -u $UID -v `pwd`:/app -v ~/.composer:/tmp/.composer -e COMPOSER_HOME=/tmp/.composer composer --ignore-platform-reqs'" >> '/home/ubuntu/.bashrc'
+}
+
+function main() {
+  loadkeys de
+
+  add_missing_repository_keys
+  install_raspberry_components
+  install_dnsmasq
+  install_docker
+  install_docker_compose
+  add_composer_alias
+}
+main
--- Setup/workstation.sh
+++ Setup/workstation.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-echo -n "Folder with mounted microsd card (/media/[username]) without slash followed and [ENTER]:"
-read FOLDER
-
 # add repository for raspbian packages
-cat <<EOT >> ${FOLDER}/writable/etc/apt/sources.list.d/raspbian-buster.list
+function create_raspbian_sourcelistfile() {
+  local folder=$1
 
-deb [arch=armhf] http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
-# Uncomment line below then 'apt-get update' to enable 'apt-get source'
-#deb-src [arch=armhf] http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
+  cat <<EOT >> ${folder}/writable/etc/apt/sources.list.d/raspbian-buster.list
+	deb [arch=armhf] http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
+	# Uncomment line below then 'apt-get update' to enable 'apt-get source'
+	#deb-src [arch=armhf] http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
 EOT
+}
 
-# add config for usb-tether
-echo "dtoverlay=dwc2" >> ${FOLDER}/system-boot/usercfg.txt
-echo "dwc2" > ${FOLDER}/writable/etc/modules-load.d/dwc2.conf
-echo "libcomposite" > ${FOLDER}/writable/etc/modules-load.d/libcomposite.conf
-echo "g_ether" > ${FOLDER}/writable/etc/modules-load.d/g_ether.conf
-echo "usb_f_ecm" > ${FOLDER}/writable/etc/modules-load.d/usb_f_ecm.conf
+# add modules for usb-tether
+function config_modules() {
+  local folder=$1
 
-cp ./Setup/Configuration/60-netcfg.yaml ${FOLDER}/writable/etc/netplan/
+  echo "dtoverlay=dwc2" >> ${folder}/system-boot/usercfg.txt
+  echo "dwc2" > ${folder}/writable/etc/modules-load.d/dwc2.conf
+  echo "libcomposite" > ${folder}/writable/etc/modules-load.d/libcomposite.conf
+  echo "g_ether" > ${folder}/writable/etc/modules-load.d/g_ether.conf
+  echo "usb_f_ecm" > ${folder}/writable/etc/modules-load.d/usb_f_ecm.conf
+}
+
+function copy_network_configuration() {
+  local folder=$1
+
+  cp ./Setup/Configuration/60-netcfg.yaml ${folder}/writable/etc/netplan/
+}
+
+function main() {
+  echo -n "Folder with mounted microsd card (/media/[username]) without slash followed and [ENTER]: "
+  read folder
+
+  create_raspbian_sourcelistfile ${folder}
+  config_modules ${folder}
+  copy_network_configuration ${folder}
+}
+main
