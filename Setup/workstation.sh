4a6cf8dc49931f19a915f45a6b333a4efd34bafc
Sebastian Fischer <typo3@evoweb.de>
2019-12-29 15:32:31 +0100

    Add mounting the smb share to /media/raspberrypi

--- Setup/workstation.sh
+++ Setup/workstation.sh
@@ -28,6 +28,30 @@ function copy_network_configuration() {
   cp ./Setup/Configuration/60-netcfg.yaml ${folder}/writable/etc/netplan/
 }
 
+function create_filemount() {
+  local user=$(who | awk '{print $1}')
+  local home_dir=$( getent passwd "${user}" | cut -d: -f6 )
+  local credentials_file="${home_dir}/.smbraspberrypi"
+  local mount_dir='/media/raspberrypi'
+
+  mkdir -p ${mount_dir}
+  chgrp users ${mount_dir}
+
+  apt install cifs-utils
+
+  echo 'The following password should be set via passwd on the Raspberry Pi.'
+  echo -n 'Please enter the password for the file mount of the Raspberry Pi: '
+  read password
+
+  touch ${credentials_file}
+  echo 'username=ubuntu' > ${credentials_file}
+  echo "password=${password}" >> ${credentials_file}
+  chmod 600 ${credentials_file}
+
+  mount -t cifs -o credentials=${credentials_file} //192.168.20.40/ubuntu ${mount_dir}
+  echo "//192.168.20.40/ubuntu ${mount_dir} cifs credentials=${credentials_file},user,noperm,noauto,_netdev 0 0" >> /etc/fstab
+}
+
 function main() {
   echo -n "Folder with mounted microsd card (/media/[username]) without slash followed and [ENTER]: "
   read folder
@@ -35,5 +59,7 @@ function main() {
   create_raspbian_sourcelistfile ${folder}
   config_modules ${folder}
   copy_network_configuration ${folder}
+  create_filemount
 }
+
 main
